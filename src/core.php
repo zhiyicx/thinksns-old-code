64308cc8624fcbafd51caafe8969476fb3b4338e
Seven <shiweidu@outlook.com>
2016-12-04 22:24:08 +0800

    上传代码～

--- src/core.php
+++ src/core.php
@@ -64,7 +64,7 @@ tsdefine('CONF_PATH', SITE_PATH.'/config');
 tsdefine('APPS_PATH', SITE_PATH.'/apps');
 tsdefine('APPS_URL', SITE_URL.'/apps');    # 应用内部图标 等元素
 
-tsdefine('ADDON_PATH', SITE_PATH.'/src/old/addons');
+tsdefine('ADDON_PATH', dirname(__FILE__).'/addons');
 
 tsdefine('DATA_PATH', SITE_PATH.'/data');
 tsdefine('DATA_URL', SITE_URL.'/data');

840160e3e41eb84a9a0409d39fc0f96b126811e8
zhangwei <942178655@qq.com>
2017-04-13 10:54:30 +0800

    微博列表附件信息在反序列之前将emoji格式化为代码

--- src/addons/model/FeedModel.class.php
+++ src/addons/model/FeedModel.class.php
@@ -1896,7 +1896,7 @@ class FeedModel extends Model
                 } else {
                     $list[$key]['client_ip'] = $list[$key]['data']['client_ip'];
                     $list[$key]['feed_data'] = $objList[$key]->data->feed_data;
-                    $_data = $var = unserialize($list[$key]['feed_data']);
+                    $_data = $var = unserialize(formatEmoji(true, $list[$key]['feed_data']));
                     //解析图片
                     if (!empty($_data['attach_id'])) {
                         $var['attachInfo'] = $objList[$key]->getImagesAttribute();
@@ -1983,7 +1983,7 @@ class FeedModel extends Model
                 } else {
                     $value['client_ip'] = $value['data']['client_ip'];
                     $value['feed_data'] = $objList[$key]->data->feed_data;
-                    $_data = $var = unserialize($value['feed_data']);
+                    $_data = $var = unserialize(formatEmoji(true, $value['feed_data']));
 
                     if (!empty($_data['attach_id'])) {
                         $var['attachInfo'] = $objList[$key]->getImagesAttribute();
